6c9d4a0acfc74977fa2ebaeee169cebd5233c5bd
Ammar ul mustafa <ammar.ul.mustafa3@gmail.com>
2024-04-27 08:53:49 +0500

    socketcontext

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,14 +1,21 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import "./App.css";
+import { AppBar, Typography } from "@mui/material";
+import VideoPlayer from "./components/VideoPlayer";
+import Options from "./components/Options";
+import Notification from "./components/Notification";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <>
-      <h1 className="text-3xl text-center m-4">Video Application</h1>
+      <AppBar position="static" color="primary" className="py-3">
+        <Typography variant="h3" align="center">
+          Video Application
+        </Typography>
+      </AppBar>
+      {/* <h1 className="text-3xl text-center m-4">Video Application</h1> */}
+      <VideoPlayer />
+      <Options>
+        <Notification />
+      </Options>
     </>
   );
 }
